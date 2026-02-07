cb64ba2f9cdd13d9e697c9fe0bc2a8b31872bd5a
Eddy G <eddyguzman322@gmail.com>
2026-02-07 16:41:38 -0500

    First few changes

---
+++ test.js
@@ -0,0 +1,36 @@
+/**
+ * Stuff to include in this about me webpage:
+ * Why is this test engineer a strong future software dev/SDET?
+ * Who am I and where am i headed?
+ ***** Intro /Hero section ******
+ * Include:
+ * Name
+ * Current role
+ * career direction
+ * one sentence that show motivation
+ * Example:
+“I’m a Test Engineer transitioning into Software Development and SDET roles, building hands-on projects to strengthen my coding, automation, and problem-solving skills.”
+* **** Background and mindset ****
+* Talk about Years or type of testing experience
+* Manual Vs automation
+* How testing shaped how you think about code
+****** Skills and current + learning ******
+* Current familiar with: Html, test case design, bug tracking, exploratory testing, bug tracking, sdlc knowledge, apis
+* Learning/in progress: JS, Git/Github, automation concepts, DOM manipulation, Basic algorithms
+***** Projects *****
+* what each project is and what you learned what tech used
+******* Why DEV/SDET ******
+* Why move beyond testing 
+* what excites you about coding?
+* Why sdet or dev fits your strength 
+****** Contacts and links *****
+* Include Github, linkedin, and email 
+
+
+** Toggle dark/light mode?
+** Expand /collapse sections
+* Dynamically load projects in an iframe?
+* Show currently learning section that updates
+* simple form validation?
+
+ */
