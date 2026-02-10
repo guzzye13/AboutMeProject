df2ac9773d1a22bb15a4927127267d922d3cc0be
Eddy G <eddyguzman322@gmail.com>
2026-02-09 22:30:22 -0500

    updated css color and js logic to close menu item when clicking outside of the nav and after clicking an href

--- menu.js
+++ menu.js
@@ -1,6 +1,26 @@
 const menuToggle = document.getElementById("menu-toggle");
+const dropdown = document.querySelector(".dropdown");
+const menuIcon = document.querySelector(".menu-icon");
 const menuLinks = document.querySelectorAll(".dropdown a");
 
+
+// Close menu when clicking outside
+document.addEventListener("mousedown", (event) => {
+    if (!menuToggle.checked) return;
+
+    const target = event.target;
+
+    if (
+        dropdown.contains(target) ||
+        menuIcon.contains(target)
+    ) {
+        return;
+    }
+
+    menuToggle.checked = false;
+});
+
+// Close menu when clicking a link
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
         menuToggle.checked=false;
