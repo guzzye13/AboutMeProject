5963f1125e74f6d34d9426c00f8af86b42950889
Eddy G <eddyguzman322@gmail.com>
2026-04-26 17:24:35 -0400

    Pushing latest changes

--- menu.js
+++ menu.js
@@ -3,27 +3,22 @@ const dropdown = document.querySelector(".dropdown");
 const menuIcon = document.querySelector(".menu-icon");
 const menuLinks = document.querySelectorAll(".dropdown a");
 
-
 // Close menu when clicking a link
-menuLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        menuToggle.checked=false;
-    });
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuToggle.checked = false;
+  });
 });
 
 // Close menu when clicking outside
 document.addEventListener("mousedown", (event) => {
-    if (!menuToggle.checked) return;
+  if (!menuToggle.checked) return;
 
-    const target = event.target;
+  const target = event.target;
 
-    if (
-        dropdown.contains(target) ||
-        menuIcon.contains(target)
-    ) {
-        return;
-    }
+  if (dropdown.contains(target) || menuIcon.contains(target)) {
+    return;
+  }
 
-    menuToggle.checked = false;
+  menuToggle.checked = false;
 });
-
