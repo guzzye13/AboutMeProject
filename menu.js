d3aedd723a4464ca1b5126cec313516da3cd32ac
Eddy G <eddyguzman322@gmail.com>
2026-04-27 20:28:43 -0400

    Changing the web app and separated pages by file

--- menu.js
+++ menu.js
@@ -1,24 +1,36 @@
-const menuToggle = document.getElementById("menu-toggle");
-const dropdown = document.querySelector(".dropdown");
-const menuIcon = document.querySelector(".menu-icon");
-const menuLinks = document.querySelectorAll(".dropdown a");
+// const menuToggle = document.getElementById("menu-toggle");
+// const dropdown = document.querySelector(".dropdown");
+// const menuIcon = document.querySelector(".menu-icon");
+// const menuLinks = document.querySelectorAll(".dropdown a");
 
-// Close menu when clicking a link
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    menuToggle.checked = false;
-  });
-});
+// // Close menu when clicking a link
+// menuLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     menuToggle.checked = false;
+//   });
+// });
 
-// Close menu when clicking outside
-document.addEventListener("mousedown", (event) => {
-  if (!menuToggle.checked) return;
+// // Close menu when clicking outside
+// document.addEventListener("mousedown", (event) => {
+//   if (!menuToggle.checked) return;
 
-  const target = event.target;
+//   const target = event.target;
 
-  if (dropdown.contains(target) || menuIcon.contains(target)) {
-    return;
-  }
+//   if (dropdown.contains(target) || menuIcon.contains(target)) {
+//     return;
+//   }
 
-  menuToggle.checked = false;
-});
+//   menuToggle.checked = false;
+// });
+//IDEAS:
+/**
+ * Include dark/light mode toggle using JS
+ * Hamburger menu
+ * Smooth scroll
+ *
+ * Typwriter effect for the header
+ * skills progress bars in the skills section
+ * Form validation for the contact form
+ *
+ *
+ */
