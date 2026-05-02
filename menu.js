a8e07667f751388b783d3d51f2953fc3039c968e
Eddy G <eddyguzman322@gmail.com>
2026-05-02 00:47:48 -0400

    added the dark mode vs light mode and some responsiveness

--- menu.js
+++ menu.js
@@ -1,36 +1,31 @@
-// const menuToggle = document.getElementById("menu-toggle");
-// const dropdown = document.querySelector(".dropdown");
-// const menuIcon = document.querySelector(".menu-icon");
-// const menuLinks = document.querySelectorAll(".dropdown a");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("darkModeToggle");
+  const modeLabel = document.getElementById("modeLabel");
+  const hamburger = document.getElementById("hamburger");
+  const mainNav = document.getElementById("main-nav");
 
-// // Close menu when clicking a link
-// menuLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     menuToggle.checked = false;
-//   });
-// });
+  toggle.addEventListener("change", function () {
+    if (this.checked) {
+      document.body.classList.add("dark-mode");
+      modeLabel.textContent = "Dark Mode Enabled";
+    } else {
+      document.body.classList.remove("dark-mode");
+      modeLabel.textContent = "Light Mode Enabled";
+    }
+  });
+  hamburger.addEventListener("click", function () {
+    mainNav.classList.toggle("open");
 
-// // Close menu when clicking outside
-// document.addEventListener("mousedown", (event) => {
-//   if (!menuToggle.checked) return;
+    const icon = hamburger.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-mark");
+  });
+});
 
-//   const target = event.target;
+hamburger.addEventListener("click", function () {
+  mainNav.classList.toggle("open");
 
-//   if (dropdown.contains(target) || menuIcon.contains(target)) {
-//     return;
-//   }
-
-//   menuToggle.checked = false;
-// });
-//IDEAS:
-/**
- * Include dark/light mode toggle using JS
- * Hamburger menu
- * Smooth scroll
- *
- * Typwriter effect for the header
- * skills progress bars in the skills section
- * Form validation for the contact form
- *
- *
- */
+  const icon = hamburger.querySelector("i");
+  icon.classList.toggle("fa-bars");
+  icon.classList.toggle("fa-mark");
+});
